418ebc31f82f44f29d985488453ee072d074992a
deep-collectionman <jmcollectionman@gmail.com>
2022-11-07 19:07:35 -0300

    Starting Project - Base App

---
+++ android/app/src/main/kotlin/com/example/salary/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.salary
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SalaryApp());
+}
+
+class SalaryApp extends StatelessWidget {
+  const SalaryApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
